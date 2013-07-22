299118612e4e5bf92b3e07c5b0acd5cc44e4015e
James Smith <james@floppy.org.uk>
2013-07-22 11:47:30 +0100

    Override Artefact::FORMATS_BY_DEFAULT_OWNING_APP with our own types

---
+++ app/decorators/models/artefact_decorator.rb
@@ -0,0 +1,7 @@
+Artefact::FORMATS_BY_DEFAULT_OWNING_APP = {
+  'publisher' => [
+    "course",
+    "news",
+    "person"
+  ]
+}
--- lib/odi_content_models.rb
+++ lib/odi_content_models.rb
@@ -5,11 +5,14 @@ require "govuk_content_models"
 begin
   module OdiContentModels
     class Engine < Rails::Engine
+      config.to_prepare do
+        Dir.glob(File.dirname(__FILE__) + "/../app/decorators/**/*_decorator*.rb").each do |c|
+          require_dependency(c)
+        end
+      end
     end
   end
 rescue NameError
   module OdiContentModels
   end
 end
-
-require "odi_content_models/artefact_patches"
