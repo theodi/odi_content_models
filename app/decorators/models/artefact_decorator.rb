f4b8135acc02490cda9a9528c34888f47c991389
James Smith <james@floppy.org.uk>
2013-07-22 11:53:26 +0100

    Include empty whitehall array in formats list
    So that GDS validators can work.

--- app/decorators/models/artefact_decorator.rb
+++ app/decorators/models/artefact_decorator.rb
@@ -3,5 +3,6 @@ Artefact::FORMATS_BY_DEFAULT_OWNING_APP = {
     "course",
     "news",
     "person"
-  ]
+  ],
+  'whitehall' => [] # Need to define this empty so that govuk_content_models validators still work. See their slug_validator.rb for reasons.
 }
