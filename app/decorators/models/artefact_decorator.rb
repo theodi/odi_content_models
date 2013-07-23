e240a98a95f211e641aa1b9b9583af06e8bb40ec
James Smith <james@floppy.org.uk>
2013-07-23 12:53:43 +0100

    Include govuk formats in valid list
    Saves making massive changes to test suites in client apps.

--- app/decorators/models/artefact_decorator.rb
+++ app/decorators/models/artefact_decorator.rb
@@ -1,3 +1,5 @@
+govuk_formats = Artefact::FORMATS_BY_DEFAULT_OWNING_APP
+
 Artefact::FORMATS_BY_DEFAULT_OWNING_APP = {
   'publisher' => [
     "course",
@@ -7,4 +9,4 @@ Artefact::FORMATS_BY_DEFAULT_OWNING_APP = {
   'whitehall' => [] # Need to define this empty so that govuk_content_models validators still work. See their slug_validator.rb for reasons.
 }
 
-Artefact::FORMATS = Artefact::FORMATS_BY_DEFAULT_OWNING_APP.values.flatten
+Artefact::FORMATS = Artefact::FORMATS_BY_DEFAULT_OWNING_APP.values.flatten + govuk_formats.values.flatten
