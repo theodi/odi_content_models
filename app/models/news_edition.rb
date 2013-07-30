b44d5008f439e80dc54a5c92c8e6a92ca45a95a4
James Smith <james@floppy.org.uk>
2013-07-30 15:21:07 +0100

    Add image attachment to news item

--- app/models/news_edition.rb
+++ app/models/news_edition.rb
@@ -1,16 +1,17 @@
 require "edition"
 
 class NewsEdition < Edition
+  include Attachable
 
   field :subtitle,      type: String
   field :body,          type: String
-  field :home_image,    type: String
-  field :main_image,    type: String
   field :featured,      type: Boolean
   
   GOVSPEAK_FIELDS = Edition::GOVSPEAK_FIELDS + [:body]
 
   @fields_to_clone = [:subtitle, :body, :home_image, :main_image]
+
+  attaches :image
 
   def whole_body
     body
