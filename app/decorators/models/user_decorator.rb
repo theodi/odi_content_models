98cc50bd6ef9d140413352c75a9d75cfa74cf0ba
pezholio <pezholio@gmail.com>
2013-09-04 13:31:52 +0100

    Add a profile to User model

---
+++ app/decorators/models/user_decorator.rb
@@ -0,0 +1,33 @@
+require "user"
+
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field "profile", type: String  
+  
+  # If no profile is selected, create one
+  after_create :create_profile
+  
+  protected
+  
+  # Create a draft profile for the user. This can then be edited at a later time
+  def create_profile
+    if self.profile.nil?
+       a = Artefact.create(
+                :name          => self.name.titleize, 
+                :slug          => self.name.parameterize,
+                :kind          => "Person", 
+                :owning_app    => "publisher", 
+                :tag_ids       => ["people"],
+                :rendering_app => "frontend"
+              )
+       a.save
+       
+       edition = Edition.find_or_create_from_panopticon_data(a._id, self, nil)
+       edition.save
+       self.profile = a.slug
+       self.save
+    end
+  end
+end
---
+++ test/models/user_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class UserTest < ActiveSupport::TestCase
+  
+  def setup
+    auth_hash = {
+      "uid" => "1234abcd",
+      "info" => {
+        "uid"     => "1234abcd",
+        "email"   => "user@example.com",
+        "name"    => "Luther Blisset"
+      },
+      "extra" => {
+        "user" => {
+          "permissions" => ["signin"]
+        }
+      }
+    }
+    @user = User.find_for_gds_oauth(auth_hash).reload
+  end
+  
+  test "should have a profile assigned" do
+    assert_equal "luther-blisset", @user.profile
+  end
+  
+  test "should create a profile" do
+    artefact = Artefact.find_by_slug(@user.profile)
+    edition = Edition.where(panopticon_id: artefact._id).first
+    refute_nil artefact
+    refute_nil edition
+  end
+  
+end
