47b66087a31eb9dbb35971ca0ba26af599e3a997
James Smith <james@floppy.org.uk>
2013-09-06 07:28:43 +0100

    Add metadata save hooks and tests

--- app/models/attachable_with_metadata.rb
+++ app/models/attachable_with_metadata.rb
@@ -20,17 +20,67 @@ module AttachableWithMetadata
       
       # Add metadata methods
       fields.map(&:to_s).each do |attachment_field|        
+
+        before_save :"update_#{attachment_field}_metadata", 
+                    :if => :"#{attachment_field}_metadata_has_changed?"
+
         ATTACHMENT_METADATA_FIELDS.each do |metadata_field|
           
-          define_method("#{attachment_field}_#{metadata_field}") do
-            instance_variable_get("@#{attachment_field}_#{metadata_field}")
+          field_name = "#{attachment_field}_#{metadata_field}"
+
+          define_method(field_name) do
+            instance_variable_get("@#{field_name}")
           end
           
-          define_method("#{attachment_field}_#{metadata_field}=") do |value|
-            instance_variable_set("@#{attachment_field}_#{metadata_field}", value)
+          define_method("#{field_name}=") do |value|
+            if instance_variable_get("@#{field_name}") != value
+              instance_variable_set("@#{attachment_field}_metadata_has_changed", true)
+              instance_variable_set("@#{field_name}", value)
+            end
           end
           
-        end      
+        end
+        
+        define_method("#{attachment_field}_metadata_hash") do
+          Hash[ATTACHMENT_METADATA_FIELDS.map do |metadata_field|
+            [metadata_field, instance_variable_get("@#{attachment_field}_#{metadata_field}")]
+          end]
+        end
+
+        define_method("#{attachment_field}_metadata_has_changed?") do
+          instance_variable_get("@#{attachment_field}_metadata_has_changed") || false
+        end
+        
+        define_method("update_#{attachment_field}_metadata") do
+          raise ApiClientNotPresent unless Attachable.asset_api_client
+          begin
+            response = Attachable.asset_api_client.update_asset(send(:"#{attachment_field}_metadata_hash"))
+            # Clear metadata tracking flag
+            instance_variable_set("@#{attachment_field}_metadata_has_changed", false)
+            # Explicit nil return so we don't return false form line above
+            nil
+          rescue StandardError
+            raise
+            errors.add("#{attachment_field}_id".to_sym, "could not be uploaded")
+          end
+        end
+        
+        define_method("upload_#{attachment_field}") do
+          raise ApiClientNotPresent unless Attachable.asset_api_client
+          begin
+            data = {:file => instance_variable_get("@#{attachment_field}_file")}
+            data.merge!(send(:"#{attachment_field}_metadata_hash")) if send(:"#{attachment_field}_metadata_has_changed?")
+            response = Attachable.asset_api_client.create_asset(data)
+            self.send("#{attachment_field}_id=", response.id.match(/\/([^\/]+)\z/) {|m| m[1] })
+            # Clear metadata tracking flag
+            instance_variable_set("@#{attachment_field}_metadata_has_changed", false)
+            # Explicit nil return so we don't return false form line above
+            nil
+          rescue StandardError
+            errors.add("#{attachment_field}_id".to_sym, "could not be uploaded")
+          end
+        end
+
       end
     end
     
