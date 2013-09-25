dca7c7f4dc412484693dbf935e0d485b3f329b93
James Smith <james@floppy.org.uk>
2013-09-25 09:56:42 +0100

    Add person object fields
    For #9

--- test/models/person_edition_test.rb
+++ test/models/person_edition_test.rb
@@ -7,18 +7,16 @@ class PersonEditionTest < ActiveSupport::TestCase
   
   should "have correct extra fields" do
     p = FactoryGirl.build(:person_edition, panopticon_id: @artefact.id)
-    p.biography = "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future."
+    p.description = "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future."
     p.twitter = "example"
     p.email = "email@example.com"
-    p.gravatar_email = "gravatar@example.com"
 
     p.safely.save!
 
     p = PersonEdition.first
-    assert_equal "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future.", p.biography
+    assert_equal "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future.", p.description
     assert_equal "example", p.twitter
     assert_equal "email@example.com", p.email
-    assert_equal "gravatar@example.com", p.gravatar_email
   end
   
   should "give a friendly (legacy supporting) description of its format" do
@@ -27,13 +25,12 @@ class PersonEditionTest < ActiveSupport::TestCase
   end
   
   context "whole_body" do
-    should "contain just the biography" do
+    should "contain just the description" do
       p = FactoryGirl.build(:person_edition,
                             :panopticon_id => @artefact.id,
-                            :biography => "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future.",
+                            :description => "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future.",
                             :twitter => "example",
-                            :email => "email@example.com",
-                            :gravatar_email => "gravatar@example.com")
+                            :email => "email@example.com")
       expected = "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future."
       assert_equal expected, p.whole_body
     end
@@ -42,17 +39,15 @@ class PersonEditionTest < ActiveSupport::TestCase
   should "clone extra fields when cloning edition" do
     person = FactoryGirl.create(:person_edition,
                           :panopticon_id => @artefact.id,
-                          :biography => "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future.",
+                          :description => "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future.",
                           :twitter => "example",
                           :email => "email@example.com",
-                          :gravatar_email => "gravatar@example.com",
                           :state => "published")
     new_person = person.build_clone
 
-    assert_equal person.biography, new_person.biography
+    assert_equal person.description, new_person.description
     assert_equal person.twitter, new_person.twitter
     assert_equal person.email, new_person.email
-    assert_equal person.gravatar_email, new_person.gravatar_email
   end
   
 end
