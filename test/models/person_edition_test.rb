66916de97eab8cd13c0e9a496dfa905b4565cfdd
pezholio <pezholio@gmail.com>
2013-10-03 15:04:34 +0100

    Add node reference to person

--- test/models/person_edition_test.rb
+++ test/models/person_edition_test.rb
@@ -10,6 +10,7 @@ class PersonEditionTest < ActiveSupport::TestCase
     p.description = "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future."
     p.twitter = "example"
     p.email = "email@example.com"
+    p.node = "birmingham"
 
     p.safely.save!
 
@@ -17,6 +18,7 @@ class PersonEditionTest < ActiveSupport::TestCase
     assert_equal "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future.", p.description
     assert_equal "example", p.twitter
     assert_equal "email@example.com", p.email
+    assert_equal "birmingham", p.node
   end
   
   should "give a friendly (legacy supporting) description of its format" do
@@ -42,12 +44,14 @@ class PersonEditionTest < ActiveSupport::TestCase
                           :description => "Selfies direct trade fap, velit deep v street art salvia laborum banjo pour-over Godard mixtape keffiyeh literally tumblr. Next level Austin sapiente ugh, voluptate cred kogi ex fixie photo booth ullamco nostrud Odd Future.",
                           :twitter => "example",
                           :email => "email@example.com",
+                          :node => "birmingham",
                           :state => "published")
     new_person = person.build_clone
 
     assert_equal person.description, new_person.description
     assert_equal person.twitter, new_person.twitter
     assert_equal person.email, new_person.email
+    assert_equal person.node, new_person.node
   end
   
 end
