91c5c79532742f3b4b217c16b9922d9fb465652d
pezholio <pezholio@gmail.com>
2013-10-07 14:31:10 +0100

    Test for tagging validation

--- test/models/artefact_test.rb
+++ test/models/artefact_test.rb
@@ -6,5 +6,12 @@ class ArtefactTest < ActiveSupport::TestCase
     a = FactoryGirl.build(:artefact, slug: "its-a-nice-day", author: "luther-blisset")
     assert a.valid?
   end
+  
+  should "return an error if no tag is set for special types" do
+    [:person, :timed_item, :asset, :article, :organization].each do |type|
+      artefact = FactoryGirl.build(:artefact, kind: type)
+      assert_equal false, artefact.valid?
+    end
+  end
     
 end
