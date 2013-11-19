5a904b16ed77d6117a0569694cdac1bb11ba3999
James Smith <james@floppy.org.uk>
2013-11-19 11:23:45 +0000

    Add organization_name field to artefact
    In order to associate content with members or startups

--- app/decorators/models/artefact_decorator.rb
+++ app/decorators/models/artefact_decorator.rb
@@ -24,6 +24,7 @@ Artefact::FORMATS = Artefact::FORMATS_BY_DEFAULT_OWNING_APP.values.flatten + gov
 class Artefact
   field "author", type: String
   field "node", type: String
+  field "organization_name", type: String
   
   validate :check_tags
   validate :check_team
