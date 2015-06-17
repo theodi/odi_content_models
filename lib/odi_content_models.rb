e0d54f80fda09336f3e2573acdf0f7cac5714da6
Giles Dring <gilesdring@gmail.com>
2015-06-17 14:56:31 +0100

    Monkey patching govuk_content_models Edition
    theodi/publisher#135
    
    When Panopticon artefact changes, Publisher edition triggers update_from_artefact, which updates
    the title, amongst other things.
    
    Removing the update to title. This action is triggered when there
    is an update to the artefact in Panopticon. Agreed with @pezolio
    on 17 June to comment out, in case VERY BAD THINGS happen.
    slug is the prime linkage, but think that section, department and
    business_proposition are mastered in Panopticon / artefact, so
    should maintain them there, and ensure that existing editions are
    updated.

--- lib/odi_content_models.rb
+++ lib/odi_content_models.rb
@@ -11,6 +11,27 @@ begin
         end
       end
     end
+    class Edition
+      def update_from_artefact(artefact)
+        ####################################################################
+        # Removing the update to title. This action is triggered when there
+        # is an update to the artefact in Panopticon. Agreed with @pezolio
+        # on 17 June to comment out, in case VERY BAD THINGS happen.
+        # slug is the prime linkage, but think that section, department and
+        # business_proposition are mastered in Panopticon / artefact, so
+        # should maintain them there, and ensure that existing editions are
+        # updated.
+        # The issue is here: https://github.com/theodi/publisher/issues/135
+        #
+
+        # self.title = artefact.name unless published?
+        self.slug = artefact.slug
+        self.section = artefact.section
+        self.department = artefact.department
+        self.business_proposition = artefact.business_proposition
+        self.save!
+      end
+    end
   end
 rescue NameError
   module OdiContentModels
