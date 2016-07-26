1c1d02093f5750223e30fef82626cec6ec7b67a3
pezholio <pezholio@gmail.com>
2016-07-26 14:43:23 +0100

    Fix tests

--- test/models/event_edition_test.rb
+++ test/models/event_edition_test.rb
@@ -4,7 +4,7 @@ class EventEditionTest < ActiveSupport::TestCase
   setup do
     @artefact = FactoryGirl.create(:artefact)
   end
-  
+
   should "have correct extra fields" do
     n = EventEdition.create(title: "Event Edition", panopticon_id: @artefact.id)
     n.start_date = 1.day.from_now
@@ -14,7 +14,7 @@ class EventEditionTest < ActiveSupport::TestCase
     n.booking_url = "http://eventbrite.com/test"
     n.hashtag = "testing"
     n.livestream = true
-    
+
     n.safely.save!
 
     n = EventEdition.first
@@ -26,12 +26,12 @@ class EventEditionTest < ActiveSupport::TestCase
     assert_equal n.hashtag, "testing"
     assert_equal n.livestream, true
   end
-  
+
   should "give a friendly (legacy supporting) description of its format" do
     event = EventEdition.new
     assert_equal "Event", event.format
   end
-  
+
   context "whole_body" do
     should "contain just the description" do
       n = EventEdition.create(:title => "Event Edition",
@@ -47,7 +47,7 @@ class EventEditionTest < ActiveSupport::TestCase
       assert_equal expected, n.whole_body
     end
   end
-  
+
   should "clone extra fields when cloning edition" do
     event = EventEdition.create(:title => "Event Edition",
                           :panopticon_id => @artefact.id,
@@ -74,89 +74,89 @@ class EventEditionTest < ActiveSupport::TestCase
 
     should "creates /events/* paths for untagged items" do
       artefact = FactoryGirl.create(:artefact)
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/events/testing', n.rendering_path
     end
 
     should "creates /lunchtime-lectures/* paths for lectures" do
-      FactoryGirl.create(:tag, :tag_id => "lunchtime-lecture", :tag_type => 'event', :title => "Lunchtime Lecture")
+      FactoryGirl.create(:tag, :tag_id => "event:lunchtime-lecture", :tag_type => 'event', :title => "Lunchtime Lecture")
       artefact = FactoryGirl.create(:artefact, :event => ['lunchtime-lecture'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/lunchtime-lectures/testing', n.rendering_path
     end
 
     should "creates /lunchtime-lectures/* paths for lectures with keyword tag as well" do
-      FactoryGirl.create(:tag, :tag_id => "lunchtime-lecture", :tag_type => 'event', :title => "Lunchtime Lecture")
-      FactoryGirl.create(:tag, :tag_id => "lunchtime-lecture", :tag_type => 'keyword', :title => "Lunchtime Lecture")
+      FactoryGirl.create(:tag, :tag_id => "event:lunchtime-lecture", :tag_type => 'event', :title => "Lunchtime Lecture")
+      FactoryGirl.create(:tag, :tag_id => "event:lunchtime-lecture", :tag_type => 'keyword', :title => "Lunchtime Lecture")
       artefact = FactoryGirl.create(:artefact, :event => ['lunchtime-lecture'], :keywords => ['lunchtime-lecture'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/lunchtime-lectures/testing', n.rendering_path
     end
 
     should "creates /meetups/* paths for meetups" do
-      FactoryGirl.create(:tag, :tag_id => "meetup", :tag_type => 'event', :title => "Meetup")
+      FactoryGirl.create(:tag, :tag_id => "event:meetup", :tag_type => 'event', :title => "Meetup")
       artefact = FactoryGirl.create(:artefact, :event => ['meetup'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/meetups/testing', n.rendering_path
     end
 
     should "creates /research-afternoons/* paths for research afternoons" do
-      FactoryGirl.create(:tag, :tag_id => "research-afternoon", :tag_type => 'event', :title => "Research Afternoon")
+      FactoryGirl.create(:tag, :tag_id => "event:research-afternoon", :tag_type => 'event', :title => "Research Afternoon")
       artefact = FactoryGirl.create(:artefact, :event => ['research-afternoon'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/research-afternoons/testing', n.rendering_path
     end
-  
+
     should "creates /challenge-series/* paths for open data challenge events" do
-      FactoryGirl.create(:tag, :tag_id => "open-data-challenge-series", :tag_type => 'event', :title => "Open Data Challenge Series")
+      FactoryGirl.create(:tag, :tag_id => "event:open-data-challenge-series", :tag_type => 'event', :title => "Open Data Challenge Series")
       artefact = FactoryGirl.create(:artefact, :event => ['open-data-challenge-series'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/challenge-series/testing', n.rendering_path
     end
 
     should "creates /roundtables/* paths for roundtables" do
-      FactoryGirl.create(:tag, :tag_id => "roundtable", :tag_type => 'event', :title => "Roundtable")
+      FactoryGirl.create(:tag, :tag_id => "event:roundtable", :tag_type => 'event', :title => "Roundtable")
       artefact = FactoryGirl.create(:artefact, :event => ['roundtable'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/roundtables/testing', n.rendering_path
     end
 
     should "creates /workshops/* paths for workshops" do
-      FactoryGirl.create(:tag, :tag_id => "workshop", :tag_type => 'event', :title => "Workshop")
+      FactoryGirl.create(:tag, :tag_id => "event:workshop", :tag_type => 'event', :title => "Workshop")
       artefact = FactoryGirl.create(:artefact, :event => ['workshop'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/workshops/testing', n.rendering_path
     end
 
     should "creates /networking-events/* paths for networking events" do
-      FactoryGirl.create(:tag, :tag_id => "networking-event", :tag_type => 'event', :title => "Networking Event")
+      FactoryGirl.create(:tag, :tag_id => "event:networking-event", :tag_type => 'event', :title => "Networking Event")
       artefact = FactoryGirl.create(:artefact, :event => ['networking-event'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/networking-events/testing', n.rendering_path
     end
 
     should "creates /panel-discussions/* paths for panel discussions" do
-      FactoryGirl.create(:tag, :tag_id => "panel-discussion", :tag_type => 'event', :title => "Panel Discussion")
+      FactoryGirl.create(:tag, :tag_id => "event:panel-discussion", :tag_type => 'event', :title => "Panel Discussion")
       artefact = FactoryGirl.create(:artefact, :event => ['panel-discussion'])
-      n = EventEdition.create(:title         => "Event", 
+      n = EventEdition.create(:title         => "Event",
                               :panopticon_id => artefact.id,
                               :slug          => "testing")
       assert_equal '/panel-discussions/testing', n.rendering_path
